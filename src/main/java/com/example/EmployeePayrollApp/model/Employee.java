fec1404ee323227afdf083e7144565ac1c604731
MuskanVishwakarmaa <muskanvishwakarma06941@gmail.com>
2025-03-17 12:19:02 +0530

    Added Validation

--- src/main/java/com/example/EmployeePayrollApp/model/Employee.java
+++ src/main/java/com/example/EmployeePayrollApp/model/Employee.java
@@ -1,9 +1,11 @@
 package com.example.EmployeePayrollApp.model;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 @Entity  // Marks this class as a database entity
 @Table(name = "employees")  // Specifies the table name
 @Data  // Generates getters, setters, toString, equals, and hashCode
@@ -14,6 +16,8 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)  // Auto-increments ID
     private Long id;
+    @NotEmpty(message = "Name cannot be empty")
+    @Pattern(regexp = "^[A-Za-z\\s]+$", message = "Name should contain only alphabets and spaces")
     private String name;
     private String department;
     private double salary;
